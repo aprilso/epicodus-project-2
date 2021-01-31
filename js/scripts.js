229a7cdaadbe706afb290dcde55fbe2e22f0868e
April Soetarman <aprilsoetarman@gmail.com>
2021-01-30 22:23:12 -0800

    fix quiz logic about semicolons

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,6 @@ $(document).ready(function(){
   $("form#quiz").submit(function(event) {
     event.preventDefault();
 
-    //const likingSemicolons = $("input:radio[name=semicolons]:checked").val();
-    //print likingSemicolons;
     const name =$("select#name").val();
     const pets = $("select#pet").val();
     const semicolons= $("input:radio[name=semicolon]:checked").val();
@@ -11,9 +9,8 @@ $(document).ready(function(){
     const games = $("input:radio[name=games]:checked").val();
     const breakfast = $("select#breakfast").val();
 
-    $("#name").show()
 
-    if (semicolons === "no" || semicolons === "neutral")  {
+    if (semicolons === "no")  {
       $('#python').show();
     } else if (games === "yes" && semicolons === "yes") {
       $('#Csharp').show();
